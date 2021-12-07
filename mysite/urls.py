55f3c84c3703566af9f5b83ca4518bbe15116f04
heidipuato <heidipuato@gmail.com>
2021-12-07 23:54:06 +0800

    calendar

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
     url(r'^tinymce/', include('tinymce.urls')),   
 ]
 
