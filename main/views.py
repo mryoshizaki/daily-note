8c38fc003c312ecf0efe5ff1bd8cb5e64c9a2c46
Gwyneth Rose Rosario <gwynethrosario@gmail.com>
2021-12-16 08:11:40 +0800

    login

--- main/views.py
+++ main/views.py
@@ -24,8 +24,8 @@ from .utils import Calendar
 
 
 # Create your views here.
-# def index(request):
-#     return redirect('login')
+def index(request):
+    return render(request,'main/login.html')
 
 @unauthenticated_user
 def loginPage(request):
